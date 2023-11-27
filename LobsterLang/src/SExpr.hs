364737a9c893986cdc44b3f6709f38a3fe44f95f
Axel Humeau <axel.humeau@epitech.eu>
2023-11-27 16:03:25 +0100

    feat: get symbol, integer and list function for SExpr

--- LobsterLang/src/SExpr.hs
+++ LobsterLang/src/SExpr.hs
@@ -3,3 +3,17 @@ data SExpr = Integer Int
             | List [SExpr]
             deriving Show
 
+getSymbol :: SExpr -> Maybe String
+getSymbol (Integer _) = Nothing
+getSymbol (List _) = Nothing
+getSymbol (Symbol s) = Just s
+
+getInteger :: SExpr -> Maybe Int
+getInteger (Integer int) = Just int
+getInteger (List _) = Nothing
+getInteger (Symbol _) = Nothing
+
+getList :: SExpr -> Maybe [SExpr]
+getList (Integer _) = Nothing
+getList (List l) = Just l
+getList (Symbol _) = Nothing
