bbc66c5c3552656952bf1ac441ef45a629ba7b37
Axel Humeau <axel.humeau@epitech.eu>
2023-11-30 14:40:59 +0100

    feat: handle functions in ast

--- LobsterLang/src/AST.hs
+++ LobsterLang/src/AST.hs
@@ -6,13 +6,13 @@ data Ast = Define String Ast
         | Integer Int
         | Symbol String
         | Boolean Bool
-        | List [Ast]
+        | Call String [Ast]
         deriving Show
 
 sexprToAst :: SExpr -> Maybe Ast
-sexprToAst (SExpr.List []) = Nothing
 sexprToAst (SExpr.List [(SExpr.Symbol "define"), (SExpr.Symbol s), t]) = (Define s) <$> sexprToAst t
-sexprToAst (SExpr.List l) = AST.List <$> sequence (map sexprToAst l)
+sexprToAst (SExpr.List (SExpr.Symbol f:xs)) = (Call f) <$> sequence (map sexprToAst xs)
+sexprToAst (SExpr.List _) = Nothing
 sexprToAst (SExpr.Integer i) = Just (AST.Integer i)
 sexprToAst (SExpr.Symbol "true") = Just (Boolean True)
 sexprToAst (SExpr.Symbol "false") = Just (Boolean False)
