04ddd66e7aa97b52f5f31e920e8c4999a3f72954
Leo Soidiki <leo.soidiki@epitech.eu>
2023-12-20 11:18:38 +0100

    feat: add error handling and more patern matching for operator and execution

--- LobsterLang/src/Vm.hs
+++ LobsterLang/src/Vm.hs
@@ -16,6 +16,7 @@ import Data.Ratio
 
 data Value = IntVal Int
            | BoolVal Bool
+           deriving (Show, Eq)
 
 instance Num Value where
   (IntVal x) + (IntVal y) = IntVal (x + y)
@@ -48,34 +49,39 @@ data Instruction = Push Value
 type Stack = [Value]
 type Inst = [Instruction]
 
-makeOperation :: Operator -> Stack -> Stack
+makeOperation :: Operator -> Stack -> Either String Stack
 makeOperation Add stack = case Stack.pop stack of
     (Just x, stack1) -> case Stack.pop stack1 of
-        (Just y, stack2) -> Stack.push stack2 (x + y)
-        (Nothing, _) -> stack
-    (Nothing, _) -> stack
+        (Just y, stack2) -> Right (Stack.push stack2 (x + y))
+        (Nothing, _) -> Left "Error : Add need two arguments"
+    (Nothing, _) -> Left "Error : Add need two arguments"
 makeOperation Subtract stack = case Stack.pop stack of
     (Just x, stack1) -> case Stack.pop stack1 of
-        (Just y, stack2) -> Stack.push stack2 (x - y)
-        (Nothing, _) -> stack
-    (Nothing, _) -> stack
+        (Just y, stack2) -> Right (Stack.push stack2 (x - y))
+        (Nothing, _) -> Left "Error : Subtract need two arguments"
+    (Nothing, _) -> Left "Error : Subtract need two arguments"
 makeOperation Multiply stack = case Stack.pop stack of
     (Just x, stack1) -> case Stack.pop stack1 of
-        (Just y, stack2) -> Stack.push stack2 (x * y)
-        (Nothing, _) -> stack
-    (Nothing, _) -> stack
+        (Just y, stack2) -> Right (Stack.push stack2 (x * y))
+        (Nothing, _) -> Left "Error : Multiply need two arguments"
+    (Nothing, _) -> Left "Error : Multiply need two arguments"
 makeOperation Divide stack = case Stack.pop stack of
     (Just x, stack1) -> case Stack.pop stack1 of
-        (Just y, stack2) -> Stack.push stack2 (x / y)
-        (Nothing, _) -> stack
-    (Nothing, _) -> stack
+        (Just y, stack2)
+            | y /= 0 -> Right (Stack.push stack2 (x / y))
+            | otherwise -> Left "Error: division by zero"
+        (Nothing, _) -> Left "Error : Divide need two arguments"
+    (Nothing, _) -> Left "Error : Divide need two arguments"
 
 
 
 exec :: Inst -> Stack -> Either String Value
-exec (Call op :xs) stack = exec xs (makeOperation op stack)
+exec (Call _:_) [] = Left "Error: stack is empty"
+exec (Call op :xs) stack = case makeOperation op stack of
+    Left err -> Left err
+    Right newstack -> exec xs newstack
 exec (Push val : xs) stack = exec xs (Stack.push stack val)
 exec (Ret : _) stack = case Stack.top stack of
     Just x -> Right x
-    Nothing -> error "error: stack is empty"
-exec [] _ = error "list no instruction found"
+    Nothing -> Left "Error: stack is empty"
+exec [] _ = Left "list no instruction found"
