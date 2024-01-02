468a2b07476fb084a2835a8db76920f1380bfb7f
Aldric <aldric.jourdain@epitech.eu>
2024-01-02 16:01:27 +0100

    style: fixing coding style in Compiler.hs (too long lines)

--- LobsterLang/src/Compiler.hs
+++ LobsterLang/src/Compiler.hs
@@ -105,58 +105,99 @@ astToInstructions (Symbol symbolName) = [PushS symbolName]
 astToInstructions (AST.Call "-" [Value value]) = [PushI (-value)] -- Probably useless
 astToInstructions (AST.Call "-" [Symbol symbolName]) = PushS symbolName : [Neg]
 astToInstructions (AST.Call "!" [Boolean bool]) = [PushB (not bool)]
-astToInstructions (AST.Call "+" args) = concatMap astToInstructions args ++ [Add]
-astToInstructions (AST.Call "-" args) = concatMap astToInstructions args ++ [Sub]
-astToInstructions (AST.Call "*" args) = concatMap astToInstructions args ++ [Mul]
-astToInstructions (AST.Call "/" args) = concatMap astToInstructions args ++ [Div]
-astToInstructions (AST.Call "%" args) = concatMap astToInstructions args ++ [Mod]
-astToInstructions (AST.Call "==" args) = concatMap astToInstructions args ++ [Eq]
-astToInstructions (AST.Call "<" args) = concatMap astToInstructions args ++ [Less]
-astToInstructions (AST.Call "<=" args) = concatMap astToInstructions args ++ [LessEq]
-astToInstructions (AST.Call ">" args) = concatMap astToInstructions args ++ [Great]
-astToInstructions (AST.Call ">=" args) = concatMap astToInstructions args ++ [GreatEq]
-astToInstructions (AST.Call "&&" args) = concatMap astToInstructions args ++ [And]
-astToInstructions (AST.Call "||" args) = concatMap astToInstructions args ++ [Or]
-astToInstructions (AST.Call "!" args) = concatMap astToInstructions args ++ [Not]
-astToInstructions (AST.Call funcName args) = concatMap astToInstructions args ++ [PushS funcName] ++ [Compiler.Call]
-astToInstructions (Define symbolName value) = let symbolValue = astToInstructions value
-                                                in [Def symbolName (length symbolValue) symbolValue]
+astToInstructions (AST.Call "+" args) =
+  concatMap astToInstructions args ++ [Add]
+astToInstructions (AST.Call "-" args) =
+  concatMap astToInstructions args ++ [Sub]
+astToInstructions (AST.Call "*" args) =
+  concatMap astToInstructions args ++ [Mul]
+astToInstructions (AST.Call "/" args) =
+  concatMap astToInstructions args ++ [Div]
+astToInstructions (AST.Call "%" args) =
+  concatMap astToInstructions args ++ [Mod]
+astToInstructions (AST.Call "==" args) =
+  concatMap astToInstructions args ++ [Eq]
+astToInstructions (AST.Call "<" args) =
+  concatMap astToInstructions args ++ [Less]
+astToInstructions (AST.Call "<=" args) =
+  concatMap astToInstructions args ++ [LessEq]
+astToInstructions (AST.Call ">" args) =
+  concatMap astToInstructions args ++ [Great]
+astToInstructions (AST.Call ">=" args) =
+  concatMap astToInstructions args ++ [GreatEq]
+astToInstructions (AST.Call "&&" args) =
+  concatMap astToInstructions args ++ [And]
+astToInstructions (AST.Call "||" args) =
+  concatMap astToInstructions args ++ [Or]
+astToInstructions (AST.Call "!" args) =
+  concatMap astToInstructions args ++ [Not]
+astToInstructions (AST.Call funcName args) =
+  concatMap astToInstructions args ++ [PushS funcName] ++ [Compiler.Call]
+astToInstructions (Define symbolName value) =
+  let symbolValue = astToInstructions value
+  in [Def symbolName (length symbolValue) symbolValue]
 
 _showInstruction :: Instruction -> Int -> String
-_showInstruction (PushI value) depth = concat (replicate depth "\t") ++ "PUSH_I " ++ show value ++ "\n"
-_showInstruction (PushB bool) depth = concat (replicate depth "\t") ++ "PUSH_B " ++ show bool ++ "\n"
-_showInstruction (PushS symbolName) depth = concat (replicate depth "\t") ++ "PUSH_S " ++ show symbolName ++ "\n"
-_showInstruction (JumpIfFalse branchOffset) depth = concat (replicate depth "\t") ++ "JUMP_IF_FALSE " ++ show branchOffset ++ "\n"
-_showInstruction Add depth = concat (replicate depth "\t") ++ "ADD" ++ "\n"
-_showInstruction Sub depth = concat (replicate depth "\t") ++ "SUB" ++ "\n"
-_showInstruction Mul depth = concat (replicate depth "\t") ++ "MUL" ++ "\n"
-_showInstruction Div depth = concat (replicate depth "\t") ++ "DIV" ++ "\n"
-_showInstruction Mod depth = concat (replicate depth "\t") ++ "MOD" ++ "\n"
-_showInstruction Eq depth = concat (replicate depth "\t") ++ "EQ" ++ "\n"
-_showInstruction Less depth = concat (replicate depth "\t") ++ "LESS" ++ "\n"
-_showInstruction LessEq depth = concat (replicate depth "\t") ++ "LESS_EQ" ++ "\n"
-_showInstruction Great depth = concat (replicate depth "\t") ++ "GREAT" ++ "\n"
-_showInstruction GreatEq depth = concat (replicate depth "\t") ++ "GREAT_EQ" ++ "\n"
-_showInstruction And depth = concat (replicate depth "\t") ++ "AND" ++ "\n"
-_showInstruction Or depth = concat (replicate depth "\t") ++ "OR" ++ "\n"
-_showInstruction Not depth = concat (replicate depth "\t") ++ "NOT" ++ "\n"
-_showInstruction Neg depth = concat (replicate depth "\t") ++ "NEG" ++ "\n"
-_showInstruction Compiler.Call depth = concat (replicate depth "\t") ++ "CALL" ++ "\n"
+_showInstruction (PushI value) depth =
+  concat (replicate depth "\t") ++ "PUSH_I " ++ show value ++ "\n"
+_showInstruction (PushB bool) depth =
+  concat (replicate depth "\t") ++ "PUSH_B " ++ show bool ++ "\n"
+_showInstruction (PushS symbolName) depth =
+  concat (replicate depth "\t") ++ "PUSH_S " ++ show symbolName ++ "\n"
+_showInstruction (JumpIfFalse branchOffset) depth =
+  concat (replicate depth "\t")
+  ++ "JUMP_IF_FALSE "
+  ++ show branchOffset ++ "\n"
+_showInstruction Add depth =
+  concat (replicate depth "\t") ++ "ADD" ++ "\n"
+_showInstruction Sub depth =
+  concat (replicate depth "\t") ++ "SUB" ++ "\n"
+_showInstruction Mul depth =
+  concat (replicate depth "\t") ++ "MUL" ++ "\n"
+_showInstruction Div depth =
+  concat (replicate depth "\t") ++ "DIV" ++ "\n"
+_showInstruction Mod depth =
+  concat (replicate depth "\t") ++ "MOD" ++ "\n"
+_showInstruction Eq depth =
+  concat (replicate depth "\t") ++ "EQ" ++ "\n"
+_showInstruction Less depth =
+  concat (replicate depth "\t") ++ "LESS" ++ "\n"
+_showInstruction LessEq depth =
+  concat (replicate depth "\t") ++ "LESS_EQ" ++ "\n"
+_showInstruction Great depth =
+  concat (replicate depth "\t") ++ "GREAT" ++ "\n"
+_showInstruction GreatEq depth =
+  concat (replicate depth "\t") ++ "GREAT_EQ" ++ "\n"
+_showInstruction And depth =
+  concat (replicate depth "\t") ++ "AND" ++ "\n"
+_showInstruction Or depth =
+  concat (replicate depth "\t") ++ "OR" ++ "\n"
+_showInstruction Not depth =
+  concat (replicate depth "\t") ++ "NOT" ++ "\n"
+_showInstruction Neg depth =
+  concat (replicate depth "\t") ++ "NEG" ++ "\n"
+_showInstruction Compiler.Call depth =
+  concat (replicate depth "\t") ++ "CALL" ++ "\n"
 _showInstruction Ret depth = concat (replicate depth "\t") ++ "RET" ++ "\n"
-_showInstruction (Def symbolName nbInstruction instructions) depth
-                  = concat (replicate depth "\t") ++ "DEF " ++ show symbolName ++ " <" ++ show nbInstruction ++ "> =\n" ++ _showInstructions instructions (depth + 1)
+_showInstruction (Def symbolName nbInstruction instructions) depth =
+  concat (replicate depth "\t") ++ "DEF " ++ show symbolName ++ " <" ++
+  show nbInstruction ++ "> =\n" ++ _showInstructions instructions (depth + 1)
 
 _showInstructions :: [Instruction] -> Int -> String
-_showInstructions instructions depth = concatMap lambda instructions where lambda x = _showInstruction x depth
+_showInstructions instructions depth =
+  concatMap lambda instructions where lambda x = _showInstruction x depth
 
 showInstructions :: [Instruction] -> IO()
 showInstructions instructions = putStr (_showInstructions instructions 0)
 
 _putOpCodeFromInstruction :: Instruction -> Put
-_putOpCodeFromInstruction instruction = putWord8 (fromIntegral (fromEnum instruction))
+_putOpCodeFromInstruction instruction =
+  putWord8 (fromIntegral (fromEnum instruction))
 
 _putString :: String -> Put
-_putString string = let byteString = BSUTF8.fromString string in putInt32le (fromIntegral (BS.length byteString)) >> putByteString byteString
+_putString string = let byteString = BSUTF8.fromString string
+  in putInt32le (fromIntegral (BS.length byteString))
+  >> putByteString byteString
 
 _putInt32 :: Int -> Put
 _putInt32 value = putInt32le (fromIntegral (value::Int))
@@ -165,10 +206,15 @@ _putBool :: Bool -> Put
 _putBool bool = putWord8 (fromIntegral (fromEnum bool))
 
 _compileInstruction :: Instruction -> Put
-_compileInstruction (PushI value) = _putOpCodeFromInstruction (PushI value) >> _putInt32 value
-_compileInstruction (PushB bool) = _putOpCodeFromInstruction (PushB bool) >> _putBool bool
-_compileInstruction (PushS symbolName) = _putOpCodeFromInstruction (PushS symbolName) >> _putString symbolName
-_compileInstruction (JumpIfFalse branchOffset) = _putOpCodeFromInstruction (JumpIfFalse branchOffset) >> _putInt32 branchOffset
+_compileInstruction (PushI value) =
+  _putOpCodeFromInstruction (PushI value) >> _putInt32 value
+_compileInstruction (PushB bool) =
+  _putOpCodeFromInstruction (PushB bool) >> _putBool bool
+_compileInstruction (PushS symbolName) =
+  _putOpCodeFromInstruction (PushS symbolName) >> _putString symbolName
+_compileInstruction (JumpIfFalse branchOffset) =
+  _putOpCodeFromInstruction (JumpIfFalse branchOffset)
+  >> _putInt32 branchOffset
 _compileInstruction Add = _putOpCodeFromInstruction Add
 _compileInstruction Sub = _putOpCodeFromInstruction Sub
 _compileInstruction Mul = _putOpCodeFromInstruction Mul
@@ -186,14 +232,21 @@ _compileInstruction Neg = _putOpCodeFromInstruction Neg
 _compileInstruction Compiler.Call = _putOpCodeFromInstruction Compiler.Call
 _compileInstruction Ret = _putOpCodeFromInstruction Ret
 _compileInstruction (Def symbolName nbInstruction instructions)
-    = _putOpCodeFromInstruction (Def symbolName nbInstruction instructions) >> _putString symbolName >> _putInt32 nbInstruction >> compileInstructions instructions
+    = _putOpCodeFromInstruction (Def symbolName nbInstruction instructions)
+    >> _putString symbolName
+    >> _putInt32 nbInstruction
+    >> compileInstructions instructions
 
 compileInstructions :: [Instruction] -> Put
 compileInstructions [instruction] = _compileInstruction instruction
-compileInstructions (instruction:instructions) = _compileInstruction instruction >> compileInstructions instructions
+compileInstructions (instruction:instructions) =
+  _compileInstruction instruction >> compileInstructions instructions
 
 writeCompiledInstructionsToFile :: String -> Put -> IO()
-writeCompiledInstructionsToFile filepath compiledInstructions = BS.writeFile filepath (BS.concat $ BSL.toChunks $ runPut compiledInstructions)
+writeCompiledInstructionsToFile filepath compiledInsts =
+  BS.writeFile filepath (BS.concat $ BSL.toChunks $ runPut compiledInsts)
 
 compile :: Ast -> String -> IO()
-compile ast filepath = writeCompiledInstructionsToFile filepath (compileInstructions (astToInstructions ast))
+compile ast filepath =
+  writeCompiledInstructionsToFile
+  filepath (compileInstructions (astToInstructions ast))
