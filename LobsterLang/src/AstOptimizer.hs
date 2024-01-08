9d2550b9da595c142f2e652bae4581f357468389
Axel Humeau <axel.humeau@epitech.eu>
2024-01-08 01:36:07 +0100

    feat: optimization of values

--- LobsterLang/src/AstOptimizer.hs
+++ LobsterLang/src/AstOptimizer.hs
@@ -12,24 +12,33 @@ import Scope (ScopeMb)
 import AstEval
 
 data AstError = Error String Ast
-              | Warning String Ast
 
-optimizeAst :: [ScopeMb] -> [Ast] -> [Either AstError Ast]
+data AstOptimised = Result Ast
+                  | Warning String Ast
+
+optimizeAst :: [ScopeMb] -> [Ast] -> [Either AstError AstOptimised]
+optimizeAst stack ((Value v):xs) = Right (Result (Value v)) : optimizeAst stack xs
+optimizeAst stack ((Boolean b):xs) = Right (Result (Boolean b)) : optimizeAst stack xs
+optimizeAst stack ((String str):xs) = Right (Result (String str)) : optimizeAst stack xs
+optimizeAst stack ((List asts):xs) = case sequence (optimizeAst stack asts) of
+    Left err -> Left err : optimizeAst stack xs
+    Right opAst -> Right (Result (List (map fromOptimised opAst))) : optimizeAst stack xs
 optimizeAst stack ((Define n ast):xs) = case optimizeAst stack [ast] of
     [Left err] -> Left err : optimizeAst stack xs
-    [Right opAst] -> Right (Define n opAst) : optimizeAst stack xs
-    _ -> Left (Warning "This situation shouldn't happen" (Define n ast)) : optimizeAst stack xs
+    [Right (Result opAst)] -> Right (Result (Define n opAst)) : optimizeAst stack xs
+    [Right (Warning mes opAst)] -> Right (Warning mes (Define n opAst)) : optimizeAst stack xs
+    _ -> Right (Warning "This situation shouldn't happen" (Define n ast)) : optimizeAst stack xs
 optimizeAst stack ((Call op asts):xs)
     | foldr ((&&) . isUnoptimizable) True asts &&
     foldr ((&&) . isValue) True asts = case evalAst stack (Call op asts) of
         (Left err, _) -> Left (Error err (Call op asts)) : optimizeAst stack xs
-        (Right (Just ast), stack') -> Right ast : optimizeAst stack' xs
-        _ -> Left (Warning "This situation shouldn't happen" (Call op asts)) : optimizeAst stack xs
-    | foldr ((&&) . isUnoptimizable) True asts = Right (Call op asts) : optimizeAst stack xs
+        (Right (Just ast), stack') -> Right (Result ast) : optimizeAst stack' xs
+        _ -> Right (Warning "This situation shouldn't happen" (Call op asts)) : optimizeAst stack xs
+    | foldr ((&&) . isUnoptimizable) True asts = Right (Result (Call op asts)) : optimizeAst stack xs
     | otherwise = case sequence (optimizeAst stack asts) of
         Left err -> Left err : optimizeAst stack xs
-        Right asts' -> optimizeAst stack (Call op asts':xs)
-optimizeAst stack (ast:xs) = Right ast : optimizeAst stack xs
+        Right asts' -> optimizeAst stack (Call op (map fromOptimised asts'):xs)
+optimizeAst stack (ast:xs) = Right (Result ast) : optimizeAst stack xs
 optimizeAst _ [] = []
 
 isUnoptimizable :: Ast -> Bool
@@ -59,3 +68,7 @@ isValue (String _) = True
 isValue (List _) = True
 isValue (FunctionValue _ _ Nothing) = True
 isValue _ = False
+
+fromOptimised :: AstOptimised -> Ast
+fromOptimised (Warning _ ast) = ast
+fromOptimised (Result ast) = ast
